a2be543125c6021495c16898fa8c79964b337ec2
StartMeUp <contact@start-me-up.fr>
2023-07-05 10:01:16 +0200

    refactor page [id] error and fetching states

--- src/pages/games/[id].js
+++ src/pages/games/[id].js
@@ -11,15 +11,16 @@ export default function GameID() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
-      {data === null || loading ? (
-        "Fetching data..."
-      ) : (
+      {loading || data === null ? <p>Fetching data...</p> : null}
+
+      {error ? <p>Error ...</p> : null}
+
+      {data ? (
         <div key={data.id} className="flex flex-col text-center gap-2">
           <div className="font-bold">{data.name}</div>
           <div dangerouslySetInnerHTML={{ __html: data.description }}></div>
         </div>
-      )}
-      {console.log(data)}
+      ) : null}
     </div>
   );
 }
