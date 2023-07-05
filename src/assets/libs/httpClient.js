658ebeae573fab1b5943da0d8da1dbb023bb699b
ToineF <antoine.foucault777@gmail.com>
2023-07-05 15:47:06 +0200

    first pass for random games button exceptions

--- src/assets/libs/httpClient.js
+++ src/assets/libs/httpClient.js
@@ -2,20 +2,28 @@ const API_Key = process.env.NEXT_PUBLIC_RAWG_API_KEY;
 
 import { Client } from "@hyper-fetch/core";
 
+const urlEnd = `?key=${API_Key}`;
+
 export const client = new Client({
-  url: `https://api.rawg.io/api/games?key=${API_Key}`,
+  url: `https://api.rawg.io/api/games`,
 });
 
 export const getGames = client.createRequest()({
-  endpoint: "",
+  endpoint: urlEnd,
 });
 
 export function getGame(page = 1) {
   return {
-    game: new Client({
-      url: `https://api.rawg.io/api/games/${page}?key=${API_Key}`,
-    }).createRequest()({
-      endpoint: "",
+    game: client.createRequest()({
+      endpoint: `/${page}${urlEnd}`,
+    }),
+  };
+}
+
+export function getGamesPage(id = 1) {
+  return {
+    page: client.createRequest()({
+      endpoint: `${urlEnd}&page=${id}`,
     }),
   };
 }
--- src/components/UI/RandomButton.js
+++ src/components/UI/RandomButton.js
@@ -1,28 +1,59 @@
-import Link from "next/link";
 import { useFetch } from "@hyper-fetch/react";
-import { getGames } from "@/assets/libs/httpClient";
+import { getGames, getGamesPage } from "@/assets/libs/httpClient";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function RandomButton({ text = "Random Game" }) {
-  const router = useRouter();
-  const { data, loading, error } = useFetch(getGames);
-
   function RandomInt(min, max) {
     return Math.floor(min + Math.random() * (max - min));
   }
+  const [gamesData, setGamesData] = useState(null);
+  const [pageData, setPageData] = useState(null);
+  const [request_switch, setRequestSwitch] = useState(0);
+
+  async function pageFetchData() {
+    const gamesNumber = gamesData.count;
+    const gamePerPage = gamesData.results.length;
+    const randomURL = RandomInt(1, Math.ceil(gamesNumber / gamePerPage) + 1);
+
+    const page_res = await fetch(
+      `https://api.rawg.io/api/games?key=${process.env.NEXT_PUBLIC_RAWG_API_KEY}&page=${randomURL}`
+    );
+    setPageData(await page_res.json());
+  }
+
+  const router = useRouter();
+
+  useEffect(() => {
+    async function fetchData() {
+      const games_res = await fetch(
+        `https://api.rawg.io/api/games?key=${process.env.NEXT_PUBLIC_RAWG_API_KEY}`
+      );
+      const games_data = await games_res.json();
+      setGamesData(games_data);
+    }
+    fetchData();
+    pageFetchData();
+  }, []);
 
   return (
     <button
       className="bg-gray-500 p-2 rounded text-gray-200 hover:bg-gray-600"
       onClick={(event) => {
         {
-          if (data === null) {
+          if (pageData === null || undefined) {
             event.preventDefault();
             return;
           }
-          const gamesNumber = data.count;
-          const randomURL = RandomInt(0, gamesNumber);
-          router.push(`/games/${randomURL}`);
+
+          const gameInPage = pageData.results.length;
+          const randomGame = RandomInt(0, gameInPage);
+          const randomGameURL = pageData.results[randomGame].id;
+
+          if (gamesData === null) return;
+          pageFetchData();
+
+          router.push(`/games/${randomGameURL}`);
         }
       }}
     >
