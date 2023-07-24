1ccaee9dc055e58176212466916ad370176c3b8c
ToineF <antoine.foucault777@gmail.com>
2023-07-24 12:05:59 +0200

    404 page interface improvement

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,10 @@
 export default function Error404() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start gap-2 p-24">
-      <div className="text-lg">404: An unexpected error occured.</div>
+    <main className="flex min-h-screen flex-col items-center justify-start gap-2 p-24 text-center">
+      <div className="text-3xl font-bold">
+        404: An unexpected error occured.
+      </div>
+      <p className="text-lg">You might have typed an invalid URL.</p>
     </main>
   );
 }
